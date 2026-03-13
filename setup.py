0f6c1b4aa1e6cfff909e50d701aab90183d0ad35
Ivo <ivolaime@gmail.com>
2026-03-12 22:26:18 -0500

    segunda prueba automatizada

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+config = {
+    'description': 'desarrollo',
+    'author': 'Ivo',
+    'url': 'URL to get it at',
+    'download_url': 'Where to download it.',
+    'author_email': 'ivolaime@gmail.com',
+    'version': '0.1',
+    'install_requires': ['pytest'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'NAME'
+}
+
+setup(**config)
